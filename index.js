e0c2f9d7da82544319a76846ab2f518c0b18cf3b
STIVENSON.DEV <96761629+StivensonSS@users.noreply.github.com>
2022-06-26 10:45:00 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const datosPersonales = {
+    nombre: "Roller",
+    edad: 33,
+    isDeveloper: true,
+    fechaNacimiento : "8/Feb/89",
+    libroFavorito : {
+        autor: "Santiago Posteguillo",
+        fecha: "1994",
+        nombreLIbro: "Trilogia Africanus el hijo del Consul",
+        url: "https://www.buscalibre.com.co/libro-trilogia-africanus/9788413143064/p/53180622"
+    }
+}
+
+console.log(datosPersonales);
